cc8c20da3df95e1a6cc6243c82cf337f08d2f917
Vinny Martinez <vinny@vinnymartinez.com>
2016-12-16 14:39:18 -0500

    this was a FUN ONEE

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  ALL = []
+
+  def initialize(opts)
+    @name = opts[:name]
+    @weight = opts[:weight]
+    @height = opts[:height]
+    ALL << self
+  end
+
+  def self.all
+    ALL
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  ALL = []
+
+  def initialize(opts)
+    @name = opts[:name]
+    @type = opts[:type]
+    @booty = opts[:booty]
+    ALL << self
+  end
+
+  def self.all
+    ALL
+  end
+
+  def self.clear
+    ALL.clear
+  end
 end
